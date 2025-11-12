ec34aec5ca212a0123f5ed2773a8e4ab9249bd08
vbreddyobulareddy <vbreddy.obulareddy@gmail.com>
2025-11-12 14:40:24 -0500

    Admin module setup is completed

---
+++ apps/admin/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@ui-veerly-monorepo/admin',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/admin',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
